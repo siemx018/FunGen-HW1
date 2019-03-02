5a5d733939e95fbd57f3eca6c42b03fa56a226ea
rothx195 <rothx195@umn.edu>
2019-03-02 16:31:22 -0600

    histos

--- hw1.py
+++ hw1.py
@@ -14,7 +14,7 @@ matrix = [i.split("\t") for i in text]
 ###########################################
 
 def part2():
-    print("Number of probes: ", len(text)-2)
+    print("Number of probes: ", len(text)-3)
     vals = [0,0]
     line1 = text[1].split('\t')
     for i in line1:
@@ -42,16 +42,52 @@ part2()
 
 def part3():
     ##I'm confused on what is required to do here.
-    hist = dict()
-    # for line in text[2::]:
-    #     cols = line.split("\t")
-    #     if(len(cols)>=1):
-    #         for col in cols[2::]:
-    #             value = float(col)
-    #             if(value in hist):
-    #                 hist[value] += 1
-    #             else:
-    #                 hist[value] = 1
+
+    buckets = []
+    for line in matrix[2:]:
+        for val in line[2:]:
+            buckets.append(float(val))
+
+    # plot.hist(buckets, 10)
+    #
+    # luckets = [np.log10(i) for i in buckets]
+    # plot.hist(luckets, 10)
+    # plot.show()
+
+    buckets = [[],[],[],[]]
+    for line in range(2, len(matrix)-1):
+        for j in range(2, 6):
+            val  = np.log10(float(matrix[line][j]))
+            buckets[j-2].append(val )
+    # for i in range(4):
+    #     plot.hist(buckets[i], 20)
+    #     plot.show()
+
+    means = []
+    for line in matrix[2::]:
+        vals = [float(i) for i in line[2:]]
+        sum = 0
+        for i in vals: sum += i
+        if(len(line[2:])> 0):
+            mean = sum/len(line[2:])
+            means.append(mean)
+    means.sort()
+    
+    cols = [ [] for i in range(288)]
+    for i in range(2,len(matrix)):
+        if(i % 1000 == 0):
+            print(i)
+        for j in range(2, len(matrix[i])):
+            cols[j].append(float(matrix[i][j]))
+        for c in cols:
+            c.sort()
+            for k in range(len(c)):
+                c[k] = means[k]
+
+    for i in range(4):
+        plot.hist(buckets[i], 20)
+        plot.show()
+part3()
 
 
 ###########################################
@@ -75,6 +111,7 @@ def part4():
         results = scipy.stats.ttest_ind(laps, unlps, equal_var = False)[1]
         dflst += [(results, probe)]
 
+    scipy.stats.wilcoxon(laps, unlps)
     dflst.sort(reverse=True, key = lambda x : x[0])
     for probe in dflst[:10]:
         print(matrix[probe[1]][1])
@@ -85,7 +122,7 @@ def part4():
         ## This is so the comments collapse with the function
         ## It's meaningless
     a = 5
-part4()
+
 
 ###########################################
 ##Part 5
