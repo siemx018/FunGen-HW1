0116c45debb6d30fed4c5a8110edb5554434ead6
rothx195 <rothx195@umn.edu>
2019-03-07 21:48:15 -0600

    I have made some updates including especially a good portion of part 5. I'd examine the Wilcoxon test I am doing. It seems shady. Things that need to be done: 2d, 3c, 4 mostly, 5c. Extra cred

--- hw1.py
+++ hw1.py
@@ -1,14 +1,13 @@
-import sys
 import matplotlib.pyplot as plot
 import os
 import numpy as np
 import scipy.stats
 
 ##To run from terminal 'python3 hw1.py wang_data.txt'
-file = sys.argv[1]
-text =  open(file).read().split('\n')
+text =  open('wang_data.txt').read().split('\n')
 matrix = [i.split("\t") for i in text]
 
+SHOW_GRAPHS = False
 ###########################################
 ##Part 2
 ###########################################
@@ -41,28 +40,35 @@ part2()
 ###########################################
 
 def part3():
-    ##I'm confused on what is required to do here.
 
+    ##Part A
     buckets = []
     for line in matrix[2:]:
         for val in line[2:]:
             buckets.append(float(val))
 
-    # plot.hist(buckets, 10)
-    #
-    # luckets = [np.log10(i) for i in buckets]
-    # plot.hist(luckets, 10)
-    # plot.show()
 
+    plot.hist(buckets, 100)
+    if(SHOW_GRAPHS):
+        plot.show()
+    luckets = [np.log10(i) for i in buckets]
+    plot.hist(luckets, 100)
+    if(SHOW_GRAPHS):
+        plot.show()
+
+
+    ## Part B
     buckets = [[],[],[],[]]
     for line in range(2, len(matrix)-1):
         for j in range(2, 6):
             val  = np.log10(float(matrix[line][j]))
             buckets[j-2].append(val )
-    # for i in range(4):
-    #     plot.hist(buckets[i], 20)
-    #     plot.show()
+    for i in range(4):
+         plot.hist(buckets[i], 100)
+         if(SHOW_GRAPHS):
+             plot.show()
 
+    ## Part C
     means = []
     for line in matrix[2::]:
         vals = [float(i) for i in line[2:]]
@@ -72,22 +78,10 @@ def part3():
             mean = sum/len(line[2:])
             means.append(mean)
     means.sort()
-    
-    cols = [ [] for i in range(288)]
-    for i in range(2,len(matrix)):
-        if(i % 1000 == 0):
-            print(i)
-        for j in range(2, len(matrix[i])):
-            cols[j].append(float(matrix[i][j]))
-        for c in cols:
-            c.sort()
-            for k in range(len(c)):
-                c[k] = means[k]
 
-    for i in range(4):
-        plot.hist(buckets[i], 20)
-        plot.show()
-part3()
+    trans = scipy.mat(matrix[2::][2::]).T
+
+
 
 
 ###########################################
@@ -96,9 +90,13 @@ part3()
 
 def part4():
 
-    ##This is for the ttest and maybe works
+    ##Part a
+    ##This is for the ttest
     dflst = []
+    willist = []
     ## For each probe we find the t-value
+
+    ##One of these is probably wrong
     for probe in range(2,len(matrix)-1):
         laps = []
         unlps = []
@@ -111,19 +109,48 @@ def part4():
         results = scipy.stats.ttest_ind(laps, unlps, equal_var = False)[1]
         dflst += [(results, probe)]
 
-    scipy.stats.wilcoxon(laps, unlps)
+        (s,p) = scipy.stats.wilcoxon(laps, unlps[:len(laps)])
+        willist += [(p, probe)]
+
+    willist.sort(reverse=True, key = lambda x : x[0])
     dflst.sort(reverse=True, key = lambda x : x[0])
     for probe in dflst[:10]:
         print(matrix[probe[1]][1])
-
+    print("\n \n")
+    for probe in willist[:10]:
+        print(matrix[probe[1]][1])
     dflst = [-np.log10(i[0]) for i in dflst]
     plot.hist(dflst)
-    plot.show()
+    if(SHOW_GRAPHS):
+        plot.show()
         ## This is so the comments collapse with the function
         ## It's meaningless
     a = 5
 
+part4()
 
 ###########################################
 ##Part 5
 ###########################################
+
+
+def part5():
+
+    ##Part A
+    filterd = []
+    for (p, probe) in willist:
+        corrected = p*(len(willist))
+        if(corrected < 0.05):
+            filterd.append((p,probe))
+    print(len(filterd))
+
+    ##Part B
+    FDR = 0.05
+    M = len(willist)
+    ffilter = 0
+    for i in range(M):
+        rank = i
+        score = (rank/M)*FDR
+        if(score < willist):
+            ffilter += 1
+    print(ffilter)
